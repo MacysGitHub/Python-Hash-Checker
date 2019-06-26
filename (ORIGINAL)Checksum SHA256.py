3f9528bb0f54ba47b5141233b840dd27240f92c8
Macy <macy.gm@gmail.com>
2019-06-26 11:10:23 -0600

    Update to UI Usability
    Added file chooser
    fixed case so capitalization won't interfere with hashtype

--- (ORIGINAL)Checksum SHA256.py
+++ (ORIGINAL)Checksum SHA256.py
@@ -2,21 +2,22 @@ import hashlib
 import binascii
 import sys
 import os
+from tkinter import filedialog
+from tkinter import *
 #from progress.bar import Bar
 
+hashTypes = ['sha256' , 'md5', 'sha1']
 
-hashTypes = ['SHA256','MD5', 'SHA1']
+sysPath = sys.path[0]
 
-filePath = raw_input("specify exact file path: \n\n")
+root = Tk()
+root.withdraw()
 
-fileType = raw_input("specify checksum type: \n")
+filePath =  filedialog.askopenfilename(initialdir = "~/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
 
-if(fileType not in hashTypes):
-   print("Invalid file type \n\n")
-fileSize = os.path.getsize(filePath)
+root.update()
 
-print("file is: " + str(fileSize) + " bytes")
-
+root.deiconify()
 
 def SHA256(filePath):
    """"This function returns the SHA-1 hash
@@ -28,7 +29,7 @@ def SHA256(filePath):
 
    # open file for reading in binary mode
    with open(filePath,'rb') as file:
-  
+
        # loop till the end of the file
       chunk = 0
       iteration = 0
@@ -39,35 +40,29 @@ def SHA256(filePath):
            # read only 1024 bytes at a time
            chunk = file.read(1024)
            #every 1024 bytes
-           iteration += 1024     
+           iteration += 1024
            h.update(chunk)
-           
-
    # return the hex representation of digest
    return h.hexdigest()
 
 
-message = SHA256(filePath)
-
-print("SHA256:\n" + message + "\n")
 
 def MD5(filePath):
    print("processing MD5...")
-   
+
    h = hashlib.md5()
 
    with open(filePath, 'rb') as file:
-      
+
       chunk = 0
-      
+
       while chunk != b'':
-         
+
          chunk = file.read(1024)
          h.update(chunk)
-         
+
       return h.hexdigest()
-message = MD5(filePath)
-print("MD5:\n" + message + "\n")
+
 
 def SHA1(filePath):
    print("SHA1 processing")
@@ -77,36 +72,30 @@ def SHA1(filePath):
    with open(filePath, 'rb') as file:
 
       chunk = 0
-      
+
       while chunk != b'':
-         
+
          chunk = file.read(1024)
          h.update(chunk)
-         
+
       return h.hexdigest()
-message = SHA1(filePath)
-print("SHA1:\n" + message + "\n")
 
+b = Button(root, text="MD5", command=SHA256)
 
-#----------------------------------------------------------------------
+#filePath = input("specify exact file path: \n")
+fileType = input("specify checksum type: \n")
+if(fileType.lower() == "md5"):
+    message = MD5(filePath)
+    print("MD5:\n" + message + "\n")
+if(fileType.lower() == "sha256"):
+    message = SHA256(filePath)
+    print("SHA256:\n" + message + "\n")
+if(fileType.lower() =="sha1"):
+    message = SHA1(filePath)
+    print("SHA1:\n" + message + "\n")
 
-#with open(filePath, 'rb') as afile:
-#   buf = afile.read(BLOCKSIZE)
-#   while len(buf) > 0:
-#       hasher.update(buf)
-#       buf = afile.read(BLOCKSIZE)
-#       if(buf != BLOCKSIZE):
-#           print(hasher.hexdigest)
-# ---------------------------------------------------------------------
-#string = binascii.hexlify(f)
-#print(string)
+fileSize = os.path.getsize(filePath)
+print("file is: " + str(fileSize) + " bytes")
 
-#def sha256(f):
-#   hash_sha256 = hashlib.sha256()
-#   with open(f, "rb") as f:
-#       for chunk in iter(lambda: f.read(4096), b""):
-#           hash_sha256.update(chunk)
-#   return hash_sha256.hexdigest()
-
-
-
+if(fileType.lower() not in hashTypes):
+    print("Invalid file type \n\n")
