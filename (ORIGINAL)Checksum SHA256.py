d07b54dd2bedae2f29a263dec5264348c45b10ae
Macy <macy.gm@gmail.com>
2019-06-26 12:04:39 -0600

    Update (ORIGINAL)Checksum SHA256.py

--- (ORIGINAL)Checksum SHA256.py
+++ (ORIGINAL)Checksum SHA256.py
@@ -1,23 +1,25 @@
 import hashlib
-import binascii
 import sys
 import os
 from tkinter import filedialog
-from tkinter import *
+import tkinter as tk
 #from progress.bar import Bar
 
 hashTypes = ['sha256' , 'md5', 'sha1']
 
 sysPath = sys.path[0]
 
-root = Tk()
+root = tk.Tk()
 root.withdraw()
 
-filePath =  filedialog.askopenfilename(initialdir = "~/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
+filePath = filedialog.askopenfilename(initialdir = "~/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
 
 root.update()
 
-root.deiconify()
+fileSize = os.path.getsize(filePath)
+print("\nfile is: " + str(fileSize) + " bytes")
+root.wm_deiconify()
+root.grid_location(200, 300)
 
 def SHA256(filePath):
    """"This function returns the SHA-1 hash
@@ -80,22 +82,18 @@ def SHA1(filePath):
 
       return h.hexdigest()
 
-b = Button(root, text="MD5", command=SHA256)
 
-#filePath = input("specify exact file path: \n")
-fileType = input("specify checksum type: \n")
-if(fileType.lower() == "md5"):
+fileType = input("specify checksum type: " + "(md5, sha1, sha256)" + "\n")
+
+if fileType.lower() == "md5":
     message = MD5(filePath)
     print("MD5:\n" + message + "\n")
-if(fileType.lower() == "sha256"):
+elif fileType.lower() == "sha256":
     message = SHA256(filePath)
     print("SHA256:\n" + message + "\n")
-if(fileType.lower() =="sha1"):
+elif fileType.lower() == "sha1":
     message = SHA1(filePath)
     print("SHA1:\n" + message + "\n")
 
-fileSize = os.path.getsize(filePath)
-print("file is: " + str(fileSize) + " bytes")
-
-if(fileType.lower() not in hashTypes):
+elif fileType.lower() not in hashTypes:
     print("Invalid file type \n\n")
